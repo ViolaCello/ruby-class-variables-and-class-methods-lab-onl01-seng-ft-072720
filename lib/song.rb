2d5203539494ad61bdf387704367c8eaee8bbbb2
Anthony Lepore <violacellonyc@gmail.com>
2020-08-04 19:15:23 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -3,14 +3,14 @@ class Song
   attr_accessor :name, :artist, :genre 
   
   @@count = 0   
-  @@genre = []
+  @@genres = []
   
   def initialize(name, artist, genre)
     @name = name 
     @artist = artist
     @genre = genre
     @@count += 1 
-    @@genre.push(genre)
+    @@genres.push(genre)
     
   end
   
@@ -18,8 +18,8 @@ def self.count
   @@count
 end
   
-  def self.genre
-    @@genre.uniq 
+  def self.genres
+    @@genres.uniq 
   end
   
 end
